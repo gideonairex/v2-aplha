d2650cabf8da0397f098940cc18e1efbb00fc4cc
Janzell Jurilla <jans.j@freshclinics.com>
2025-04-08 11:53:06 +0800

    feat: add api

---
+++ apps/api/server.ts
@@ -0,0 +1,9 @@
+import { serve } from '@hono/node-server'
+import app from './index'
+
+serve({
+fetch: app.fetch,
+port: 3001}
+, (info) => {
+  console.log(`🚀 Hono server running at http://localhost:${info.port}`)
+})
